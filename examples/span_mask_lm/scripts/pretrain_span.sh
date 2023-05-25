811a67c74d824eae33b19ad3d01ca0a70a7a9745
jiaohui <68314259+jiaohuix@users.noreply.github.com>
2023-05-25 21:22:56 +0800

    Update pretrain_span.sh

--- examples/span_mask_lm/scripts/pretrain_span.sh
+++ examples/span_mask_lm/scripts/pretrain_span.sh
@@ -11,7 +11,7 @@ fairseq-train --task span_masked_lm --noise-density $density --mean-noise-span-l
   $DATA  --save-dir $SAVE \
   --arch transformer_iwslt_de_en --share-all-embeddings \
   --dropout 0.1 \
-  --optimizer adam --adam-betas '(0.9, 0.98)' --weight-decay 0.01 --clip-norm 0.0 \
+  --optimizer adam --adam-betas '(0.9, 0.98)' --weight-decay 0.01 --clip-norm 1.0 \
   --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
   --tokens-per-sample 512 --sample-break-mode none \
   --max-tokens $tokens --update-freq 16 \

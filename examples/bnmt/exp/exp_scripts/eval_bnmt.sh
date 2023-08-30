8b81dce1bad7ea05f1af7502ee5753ca0cf6a068
jiaohui <68314259+jiaohuix@users.noreply.github.com>
2023-08-30 13:07:08 +0800

    Update eval_bnmt.sh

--- examples/bnmt/exp/exp_scripts/eval_bnmt.sh
+++ examples/bnmt/exp/exp_scripts/eval_bnmt.sh
@@ -1,5 +1,5 @@
 data=$1
 ckpt=$2
-fairseq-generate  $data --user-dir bert_nmt \
+fairseq-generate  $data --user-dir bert_nmt --task bert_nmt  \
     --path $ckpt \
     --beam 5 --batch-size 128 --remove-bpe

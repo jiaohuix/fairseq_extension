57b323bc7810cb2696a3e98222a01c5f186dfb7e
jiaohui <68314259+jiaohuix@users.noreply.github.com>
2023-08-30 12:43:06 +0800

    Update model_qb.py

--- examples/bnmt/bert_nmt/model_qb.py
+++ examples/bnmt/bert_nmt/model_qb.py
@@ -5,6 +5,7 @@ import logging
 import math
 import torch
 import torch.nn as nn
+from torch import Tensor
 from fairseq.modules import PositionalEmbedding
 from fairseq.modules import LayerNorm, MultiheadAttention
 from fairseq.modules.transformer_layer import TransformerDecoderLayerBase
@@ -53,7 +54,7 @@ class QBlock(TransformerDecoderLayerBase):
     def __init__(
         self, cfg, no_encoder_attn=False, add_bias_kv=False, add_zero_attn=False
     ):
-        super().__init__(cfg, no_encoder_attn, add_bias_kv, add_zero_attn)
+        super(QBlock,self).__init__(cfg, no_encoder_attn, add_bias_kv, add_zero_attn)
 
     # def build_fc1(self, input_dim, output_dim, q_noise, qn_block_size):
     #     return quant_noise(nn.Linear(input_dim, output_dim), q_noise, qn_block_size)
@@ -91,7 +92,156 @@ class QBlock(TransformerDecoderLayerBase):
             xformers_att_config=cfg.encoder.xformers_att_config,
         )
 
+    # def forward(*args,**kwargs):
+    #     print("in forward--------------------------")
+    #     return super().forward(*args,**kwargs)
 
+    def forward(
+        self,
+        x,
+        encoder_out: Optional[torch.Tensor] = None,
+        encoder_padding_mask: Optional[torch.Tensor] = None,
+        incremental_state: Optional[Dict[str, Dict[str, Optional[Tensor]]]] = None,
+        prev_self_attn_state: Optional[List[torch.Tensor]] = None,
+        prev_attn_state: Optional[List[torch.Tensor]] = None,
+        self_attn_mask: Optional[torch.Tensor] = None,
+        self_attn_padding_mask: Optional[torch.Tensor] = None,
+        need_attn: bool = False,
+        need_head_weights: bool = False,
+    ):
+        """
+        Args:
+            x (Tensor): input to the layer of shape `(seq_len, batch, embed_dim)`
+            encoder_padding_mask (ByteTensor, optional): binary
+                ByteTensor of shape `(batch, src_len)` where padding
+                elements are indicated by ``1``.
+            need_attn (bool, optional): return attention weights
+            need_head_weights (bool, optional): return attention weights
+                for each head (default: return average over heads).
+
+        Returns:
+            encoded output of shape `(seq_len, batch, embed_dim)`
+        """
+        if need_head_weights:
+            need_attn = True
+
+        residual = x
+        if self.normalize_before:
+            x = self.self_attn_layer_norm(x)
+        if prev_self_attn_state is not None:
+            prev_key, prev_value = prev_self_attn_state[:2]
+            saved_state: Dict[str, Optional[Tensor]] = {
+                "prev_key": prev_key,
+                "prev_value": prev_value,
+            }
+            if len(prev_self_attn_state) >= 3:
+                saved_state["prev_key_padding_mask"] = prev_self_attn_state[2]
+            assert incremental_state is not None
+            self.self_attn._set_input_buffer(incremental_state, saved_state)
+        _self_attn_input_buffer = self.self_attn._get_input_buffer(incremental_state)
+        if self.cross_self_attention and not (
+            incremental_state is not None
+            and _self_attn_input_buffer is not None
+            and "prev_key" in _self_attn_input_buffer
+        ):
+            if self_attn_mask is not None:
+                assert encoder_out is not None
+                self_attn_mask = torch.cat(
+                    (x.new_zeros(x.size(0), encoder_out.size(0)), self_attn_mask), dim=1
+                )
+            if self_attn_padding_mask is not None:
+                if encoder_padding_mask is None:
+                    assert encoder_out is not None
+                    encoder_padding_mask = self_attn_padding_mask.new_zeros(
+                        encoder_out.size(1), encoder_out.size(0)
+                    )
+                self_attn_padding_mask = torch.cat(
+                    (encoder_padding_mask, self_attn_padding_mask), dim=1
+                )
+            assert encoder_out is not None
+            y = torch.cat((encoder_out, x), dim=0)
+        else:
+            y = x
+
+        x, attn = self.self_attn(
+            query=x,
+            key=y,
+            value=y,
+            key_padding_mask=self_attn_padding_mask,
+            incremental_state=incremental_state,
+            need_weights=False,
+            attn_mask=self_attn_mask,
+        )
+        if self.c_attn is not None:
+            tgt_len, bsz = x.size(0), x.size(1)
+            x = x.view(tgt_len, bsz, self.nh, self.head_dim)
+            x = torch.einsum("tbhd,h->tbhd", x, self.c_attn)
+            x = x.reshape(tgt_len, bsz, self.embed_dim)
+        if self.attn_ln is not None:
+            x = self.attn_ln(x)
+        x = self.dropout_module(x)
+        x = self.residual_connection(x, residual)
+        if not self.normalize_before:
+            x = self.self_attn_layer_norm(x)
+
+        if self.encoder_attn is not None and encoder_out is not None:
+            residual = x
+            if self.normalize_before:
+                x = self.encoder_attn_layer_norm(x)
+            if prev_attn_state is not None:
+                prev_key, prev_value = prev_attn_state[:2]
+                saved_state: Dict[str, Optional[Tensor]] = {
+                    "prev_key": prev_key,
+                    "prev_value": prev_value,
+                }
+                if len(prev_attn_state) >= 3:
+                    saved_state["prev_key_padding_mask"] = prev_attn_state[2]
+                assert incremental_state is not None
+                self.encoder_attn._set_input_buffer(incremental_state, saved_state)
+
+            x, attn = self.encoder_attn(
+                query=x,
+                key=encoder_out,
+                value=encoder_out,
+                key_padding_mask=encoder_padding_mask,
+                incremental_state=incremental_state,
+                static_kv=True,
+                need_weights=need_attn or (not self.training and self.need_attn),
+                need_head_weights=need_head_weights,
+            )
+            x = self.dropout_module(x)
+            x = self.residual_connection(x, residual)
+            if not self.normalize_before:
+                x = self.encoder_attn_layer_norm(x)
+
+        residual = x
+        if self.normalize_before:
+            x = self.final_layer_norm(x)
+
+        x = self.activation_fn(self.fc1(x))
+        x = self.activation_dropout_module(x)
+        if self.ffn_layernorm is not None:
+            x = self.ffn_layernorm(x)
+        x = self.fc2(x)
+        x = self.dropout_module(x)
+        if self.w_resid is not None:
+            residual = torch.mul(self.w_resid, residual)
+        x = self.residual_connection(x, residual)
+        if not self.normalize_before:
+            x = self.final_layer_norm(x)
+        if self.onnx_trace and incremental_state is not None:
+            saved_state = self.self_attn._get_input_buffer(incremental_state)
+            assert saved_state is not None
+            if self_attn_padding_mask is not None:
+                self_attn_state = [
+                    saved_state["prev_key"],
+                    saved_state["prev_value"],
+                    saved_state["prev_key_padding_mask"],
+                ]
+            else:
+                self_attn_state = [saved_state["prev_key"], saved_state["prev_value"]]
+            return x, attn, self_attn_state
+        return x, attn, None
 
 
 # 参数写一个类。
@@ -138,9 +288,9 @@ class QFormer(nn.Module):
         #     vdim=bert_dim
         # )
         q_layer_ls = [QBlock(cfg=self.cfg) for _ in range(self.q_layers)]
-        self.qformer = nn.ModuleList(q_layer_ls)
+        self.q_layers = nn.ModuleList(q_layer_ls)
 
-    def forward(self, bert_out, bert_mask=None): #
+    def forward(self, bert_out, bert_pad_mask=None): #
         # x: bert输出
         bsz = bert_out.shape[0]
         x = torch.arange(0, self.n_query).unsqueeze(0).expand(bsz, -1)
@@ -150,7 +300,9 @@ class QFormer(nn.Module):
         q = q.transpose(0, 1)
         bert_out = bert_out.transpose(0, 1)
         # input encoder_out encoder_pad
-        q, attn_weights = self.qformer(q, bert_out)
+        for q_layer in self.q_layers:
+            out = q_layer(q, bert_out, bert_pad_mask) # x,encoder_out,encoder_padding_mask
+            q = out[0]
         q = q.transpose(0, 1)
 
         # if self.ffn is not None:
@@ -185,7 +337,7 @@ class BertNMTEncoder(TransformerEncoderBase):
         self.qformer = QFormer(cfg)
 
     def forward_bert(self, bert_input):
-        if bert_input is None: return bert_input
+        if bert_input is None: return bert_input,None
         # 1. forward bert
         bert_pad_id = self.bert_tokenizer.convert_tokens_to_ids(self.bert_tokenizer.pad_token)
         bert_cls_id = self.bert_tokenizer.convert_tokens_to_ids(self.bert_tokenizer.cls_token)
@@ -199,11 +351,11 @@ class BertNMTEncoder(TransformerEncoderBase):
 
         bert_out = self.bert_encoder(bert_input, attention_mask=~bert_encoder_padding_mask)[0]  # 直接获取最后一层输出，没返回n层states
 
-        return bert_out
+        return bert_out, bert_encoder_padding_mask
 
-    def fuse_embedding(self, nmt_embed, bert_embed):
+    def fuse_embedding(self, nmt_embed, bert_embed, bert_pad_mask=None):
         ''' qformer压缩bert输出,然后和nmt输入拼接,拼接右边,因为left-pad source(src tokens和length需要修改)'''
-        q_embed = self.qformer(bert_embed)
+        q_embed = self.qformer(bert_embed, bert_pad_mask)
         fused_embed = torch.cat([nmt_embed, q_embed], dim=1)  # [bsz,nmt_len,dim]->[bsz,nmt_len+n_query, dim]
         return fused_embed
 
@@ -220,9 +372,9 @@ class BertNMTEncoder(TransformerEncoderBase):
 
         ### BERT EMBED ###
         if bert_input is not None:
-            bert_out = self.forward_bert(bert_input)
-            x = self.fuse_embedding(nmt_embed=x, bert_embed=bert_out)
-            embed = self.fuse_embedding(nmt_embed=embed, bert_embed=bert_out)
+            bert_out,bert_encoder_padding_mask = self.forward_bert(bert_input)
+            x = self.fuse_embedding(nmt_embed=x, bert_embed=bert_out, bert_pad_mask=bert_encoder_padding_mask)
+            embed = self.fuse_embedding(nmt_embed=embed, bert_embed=bert_out, bert_pad_mask=bert_encoder_padding_mask)
 
         if self.layernorm_embedding is not None:
             x = self.layernorm_embedding(x)

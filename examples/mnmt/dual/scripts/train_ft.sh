381561c982039fc8dbc5055c29743023d8f2272f
tyymi <729390436@qq.com>
2024-04-21 22:27:25 +0800

    修改ft的epoch数量

--- examples/mnmt/dual/scripts/train_ft.sh
+++ examples/mnmt/dual/scripts/train_ft.sh
@@ -17,14 +17,14 @@ wandb_run_name=dualFtTransformer-${src}-${tgt}
 export WANDB_NAME=$wandb_run_name
 
 # --max-update 100000
-epochs=20
+epochs=10
 mkdir -p $save
 CUDA_VISIBLE_DEVICES=0 fairseq-train --fp16 -s src -t tgt \
     $data  --save-dir $save --max-epoch $epochs \
     --arch $arch --share-all-embeddings \
     --reset-optimizer --reset-meters --reset-dataloader --reset-lr-scheduler --restore-file $ckpt  \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
-    --lr 5e-4  --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+    --lr 1e-4  --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --dropout 0.3 --weight-decay 0.0001 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --max-tokens 8192 --update-freq 1  \
